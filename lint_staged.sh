9c095b895217ae2408108d0f8eab22139347565d
genndy007 <kochevgenazp@gmail.com>
2022-06-24 22:24:23 +0300

    Add echoes to hook for understanding

--- lint_staged.sh
+++ lint_staged.sh
@@ -1,6 +1,7 @@
+echo "Running black and mypy"
 for file_path in $(git diff --cached --name-only); do
   [[ "$file_path" =~ ^.*\.py$ ]] && black --check $file_path && mypy $file_path
 done
-
+echo "Running flake8"
 flake8
 exit 0
